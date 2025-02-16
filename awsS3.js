907abe73745d1b156d7342225dbc627a54b6e6cc
EdyBe <edy.belingher@spark.school>
2025-02-17 11:36:39 +1300

    Update awsS3.js

--- awsS3.js
+++ awsS3.js
@@ -1,13 +1,12 @@
-const AWS = require('aws-sdk');
+const { S3Client } = require('@aws-sdk/client-s3');
 
-// Configure AWS with your access and secret key.
-AWS.config.update({
+// Create S3 service client object
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION // e.g., 'us-east-1'
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 });
-
-// Create S3 service object
-const s3 = new AWS.S3();
 
 module.exports = s3;
